67d674e45d23c253afa09a04290cdf0a795f7400
David Banks <dave@hoglet.com>
2025-02-06 13:27:23 +0000

    Run some further tests at 46.875KHz -> 48KHz

--- BUILD.sh
+++ BUILD.sh
@@ -26,6 +26,31 @@ do
     else
         channels=1
     fi
+    ./build/test ${infile} ${outfile} ${channels} 1000
+done
 
-    ./build/test ${infile} ${outfile} ${channels} 100
+gcc -DTEST_46875_48000 src/test.c src/tinywav.c -o build/test
+
+mkdir -p build/46875
+
+for i in \
+    Sine_1KHz_0dB \
+    Sine_10KHz_0dB \
+    Sine_1KHz_60dB \
+    Sine_10KHz_60dB \
+    Square_1KHz_6dB \
+    Square_10KHz_6dB \
+    Swept \
+    assault_iir
+do
+    infile=tests/46875/${i}_${suffix}
+    outfile=build/46875/${i}_${suffix}
+    echo ${infile}
+    if [ "$i" == "assault_iir" ]; then
+        channels=2
+    else
+        channels=1
+    fi
+
+    ./build/test ${infile} ${outfile} ${channels} 1000
 done
