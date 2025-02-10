cc844644cd1525369a15adc07c6a9b0c8699e3f9
David Banks <dave@hoglet.com>
2025-02-10 18:48:43 +0000

    Gnuplot with small points

--- simulation/simulate.sh
+++ simulation/simulate.sh
@@ -9,11 +9,11 @@ ghdl -a ${OPTS} ../vhdl/coeff_rom.vhd
 ghdl -a ${OPTS} ../vhdl/sample_rate_converter.vhd
 ghdl -a ${OPTS} sample_rate_converter_tb.vhd
 ghdl -e ${OPTS} sample_rate_converter_tb
-ghdl -r sample_rate_converter_tb --vcd="audio.vcd" --stop-time=5ms | tee audio.log
+ghdl -r sample_rate_converter_tb --vcd="audio.vcd" --stop-time=3ms | tee audio.log
 cut -d\) -f2- audio.log  | cut -c2- | awk '{print $1}' | nl > audio.plot
 if [ -z "$DISPLAY" ]; then
     echo Display not set, skipping gtkwave
 else
-    gnuplot -e "plot 'audio.plot' with lp" -p
+    gnuplot -e "plot 'audio.plot' with lp pt 7 ps 0.2" -p
     gtkwave audio.vcd sample_rate_converter.gtkw
 fi
